931c70dd2e7e6c354f6ce9a914ec77f861a216f5
Abhiram98 <bellur.abhiram@gmail.com>
2024-01-26 16:50:00 -0700

    code to create links for dependencies

--- to_eclipse.py
+++ to_eclipse.py
@@ -59,7 +59,7 @@ def convert(fullxml):
 @click.command()
 @click.option("--project_dir", help="Location of project to create "
                                     ".project and .classpath files for")
-def create_project_classpath():
+def create_project_classpath(project_dir):
     # TODO: recurse through platform/*
     # Find all src folders.
     # if there is a "test" in the folder, exclude.
@@ -67,11 +67,61 @@ def create_project_classpath():
     # Add to .project
     rootdir = 'projects/intellij-community/platform'
 
+    project_xml = ET.parse("base.project")
+    classpath_xml = ET.parse("base.classpath")
+
+    sub_projects = []
+    count = 0
+    linked_resources = ET.fromstring("<linkedResources></linkedResources>")
     for subdir, dirs, files in os.walk(rootdir):
-        for file in files:
-            print(os.path.join(subdir, file))
+        if 'src' in dirs:
+            count+=1
+            sub_projects.append(os.path.join(subdir, 'src'))
+            if 'test' in subdir:
+                continue
+
+            link_name = "-".join(subdir.split('intellij-community/')[1].split('/'))
+            if link_name=='platform-impl':
+                continue
+            fullpath = os.path.abspath(os.path.join(subdir, 'src'))
+            project_out = f"""
+<link>
+    <name>{link_name}</name>
+    <type>2</type>
+    <location>{fullpath}</location>
+</link>
+            """
+            x = ET.fromstring(project_out)
+            linked_resources.append(x)
+
+            classpath_out = f'<classpathentry kind="src" path="{link_name}"/>'
+            c = ET.fromstring(classpath_out)
+            classpath_xml.getroot().insert(1, c)
+
+    project_xml.getroot().append(linked_resources)
+
+    with open(f"{project_dir}/.project", 'wb') as f:
+        project_xml.write(f)
+    with open(f"{project_dir}/.classpath", 'wb') as f:
+        classpath_xml.write(f)
+
+    with open(f"projects/.intellij-communityproject", 'wb') as f:
+        project_xml.write(f)
+    with open(f"projects/.intellij-communityclasspath", 'wb') as f:
+        classpath_xml.write(f)
+
+    with open(f"test.project", 'wb') as f:
+        project_xml.write(f)
+    with open(f"test.classpath", 'wb') as f:
+        classpath_xml.write(f)
+
+
+    print(sub_projects)
+
 
     return
 
 if __name__ == '__main__':
-    pass
+    create_project_classpath(["--project_dir",
+                              "projects/intellij-community/platform/platform-impl"],
+                             standalone_mode=False)
